0df4d148e88c521a80d9f070fe3cfe8b53fd4a34
Gar <gar@lonelydatu.com>
2019-05-29 11:25:02 -0400

    all

--- dev/std_300x250/_bundled/main.js
+++ dev/std_300x250/_bundled/main.js
@@ -3,15 +3,19 @@
 
 TweenLite.defaultEase = Power3.easeInOut;
 
-var tl = new TimelineMax();
-tl.set(".frame1", { opacity: 1 });
+function start() {
+	var tl = new TimelineMax();
+	tl.set(".frame1", { opacity: 1 });
 
-tl.from(".t1a", 2, { filter: "blur(2px)", x: "-=90", opacity: 0, ease: Power4.easeOut }, 0);
-tl.from(".t1b", 2, { filter: "blur(2px)", x: "+=90", opacity: 0, ease: Power4.easeOut }, 0);
-tl.from(".t2", 1, { opacity: 0 }, "-=.8");
-tl.from(".bg_glow", 3, { y: "+=111", opacity: 0, ease: Power2.easeInOut }, 0);
-tl.to(".box", 2, { opacity: 0, ease: Power3.easeInOut }, 0);
-tl.to(".bg", 4, { scale: .7, opacity: 1, ease: Power3.easeOut }, 0);
+	tl.from(".t1a", 2, { filter: "blur(2px)", x: "-=90", opacity: 0, ease: Power4.easeOut }, 0);
+	tl.from(".t1b", 2, { filter: "blur(2px)", x: "+=90", opacity: 0, ease: Power4.easeOut }, 0);
+	tl.from(".t2", 1, { opacity: 0 }, "-=.8");
+	tl.from(".bg_glow", 3, { y: "+=111", opacity: 0, ease: Power2.easeInOut }, 0);
+	tl.to(".box", 2, { opacity: 0, ease: Power3.easeInOut }, 0);
+	tl.to(".bg", 4, { scale: .7, opacity: 1, ease: Power3.easeOut }, 0);
+}
+
+start();
 
 module.exports = {};
 
