5bc0836fa00536d43af121630da850bb9a58431a
Gar <gar@lonelydatu.com>
2019-05-29 09:37:51 -0400

    one

--- dev/std_300x250/_bundled/log-free.js
+++ dev/std_300x250/_bundled/log-free.js
@@ -1,6 +1,17 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
+TweenLite.defaultEase = Power3.easeInOut;
+
+var tl = new TimelineMax();
+
+tl.from(".t1a", 2, { filter: "blur(2px)", x: "-=90", opacity: 0, ease: Power4.easeOut }, 0);
+tl.from(".t1b", 2, { filter: "blur(2px)", x: "+=90", opacity: 0, ease: Power4.easeOut }, 0);
+tl.from(".t2", 1, { opacity: 0 }, "-=.8");
+tl.from(".bg_glow", 3, { y: "+=111", opacity: 0, ease: Power2.easeInOut }, 0);
+tl.to(".box", 2, { opacity: 0, ease: Power3.easeInOut }, 0);
+tl.to(".bg", 4, { scale: .7, opacity: 1, ease: Power3.easeOut }, 0);
+
 module.exports = {};
 
 },{}]},{},[1])
